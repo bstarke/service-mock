0ac01a6cf80b0c5efa2f8d98edfcd185814935b2
Brad Starkenberg <brad@starkenberg.net>
2018-09-18 09:10:20 -0400

    Fix classname in Application file

--- src/main/kotlin/io/pivotal/services/mock/Application.kt
+++ src/main/kotlin/io/pivotal/services/mock/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class FuseApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<FuseApplication>(*args)
+    runApplication<Application>(*args)
 }
